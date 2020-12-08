b92c607eb0b3fd55852bf24be73c694ee7514f3a
Hugo Ledoux <h.ledoux@tudelft.nl>
2020-12-08 16:12:15 +0100

    Fix for the new .sma output, with scaling integrated

--- sstvis.py
+++ sstvis.py
@@ -1,6 +1,7 @@
 import ast
 import fileinput
 import os
+import math
 
 # prevent pygame from printing their welcome message
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
@@ -13,24 +14,24 @@ black = (0, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
-window_dimensions = (2560, 1380)  # Screen resolution to use
+window_dimensions = (1200, 800)  # Screen resolution to use
 
 # Current setup uses 0,0 -> window_dimensions as size, so have to 'correct' the coords to be closer to 0
-x_correction = 84600
-y_correction = 447000
+x_correction = 0 #bbox-minx
+y_correction = 0 #bbox-miny
 
-x_scale = y_scale = 13  # Enlarging the triangles on the screen
+scale = 1  # Enlarging the triangles on the screen
 
 # More granular correction factor, isn't affected by the scale parameter
-x_offset = 0
-y_offset = 0
+x_offset = 10
+y_offset = 10
 
 # Higher frequency is less updates, lower frequency is more updates (it's a x % frequency == 0)
 UPDATE_FREQUENCY = 1000
 
 # Only updates every nth triangle, increases clarity in high density datasets
 # Can also put this to 1 and make the scaling factor larger
-THINNING_FACTOR = 4
+THINNING_FACTOR = 1
 
 screen = pygame.display.set_mode(window_dimensions)
 screen.fill(white)
@@ -38,11 +39,15 @@ screen.fill(white)
 pygame.display.set_caption('SSTVis')
 pygame.display.flip()
 
+v_count = 1
+
 
 class Vertex:
     def __init__(self, x, y, z):
-        self.x = (float(x) - x_correction) * x_scale + x_offset
-        self.y = (float(y) - y_correction) * y_scale + y_offset
+        # print(x,y,z)
+        # print(x_correction)
+        self.x = (float(x) - x_correction) * scale + x_offset
+        self.y = (float(y) - y_correction) * scale + y_offset
         self.z = float(z)
 
         # Easy way to check if the offsets need adjusting; if these are larger than your window_dimensions, it's not going to show up.
@@ -55,30 +60,47 @@ def process_line(line, count):
     if split_line[0] == "#":
         return
 
-    elif split_line[0] == "v":
-        vertex_id = split_line[1]
-        x = split_line[2]
-        y = split_line[3]
-        z = split_line[4]
+    elif split_line[0] == "b":
+        # return
+        global x_correction 
+        x_correction = float(split_line[1])
+        global y_correction 
+        y_correction = float(split_line[2])
 
-        vertices[vertex_id] = Vertex(x, y, z)
+        deltax = float(split_line[3]) - x_correction
+        deltay = float(split_line[4]) - y_correction
+        delta = deltax
+        if (deltay < deltax):
+            delta = deltay
+
+        global scale
+        scale = math.floor(window_dimensions[1] / delta)
+
+
+    elif split_line[0] == "v":
+        x = split_line[1]
+        y = split_line[2]
+        z = split_line[3]
+        global v_count
+        vertices[int(v_count)] = Vertex(x, y, z)
+        v_count += 1 
 
     elif split_line[0] == "f":
-        f1 = split_line[1]
-        f2 = split_line[2]
-        f3 = split_line[3]
+        # print (split_line)
+        f1 = int(split_line[1])
+        f2 = int(split_line[2])
+        f3 = int(split_line[3])
 
         if count % THINNING_FACTOR == 0:
-            pygame.draw.lines(screen, black, False, ((vertices[f1].x, vertices[f1].y), (vertices[f2].x, vertices[f2].y), (vertices[f3].x, vertices[f1].y)))
+            pygame.draw.lines(screen, black, True, ((vertices[f1].x, vertices[f1].y), (vertices[f2].x, vertices[f2].y), (vertices[f3].x, vertices[f3].y)))
 
         if count % UPDATE_FREQUENCY == 0:
             pygame.display.update()
 
     elif split_line[0] == "x":
-        pass
-        # finalized_vertex = split_line[1]
-        # adjacent_vertices = ast.literal_eval(" ".join(split_line[2:]))
-        # del vids[finalized_vertex]  # FIXME: currently removing vertex that is still used! (sstdt)
+        v = int(split_line[1])
+        del vertices[v]
+        
 
     else:
         print("Unknown character in stream!")
@@ -101,7 +123,7 @@ if __name__ == "__main__":
         process_line(stdin_line, count)
 
         # Python print already adds new line
-        print(stdin_line.rstrip("\n"))
+        # print(stdin_line.rstrip("\n"))
 
         count += 1
 

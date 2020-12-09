f02615aa28dde2da1eb16b6d71d3d8ccbacd53f9
Hugo Ledoux <h.ledoux@tudelft.nl>
2020-12-09 08:23:49 +0100

    Add a red bbox in the canvas

--- sstvis.py
+++ sstvis.py
@@ -17,17 +17,16 @@ blue = (0, 0, 255)
 window_dimensions = (1200, 800)  # Screen resolution to use
 
 # Current setup uses 0,0 -> window_dimensions as size, so have to 'correct' the coords to be closer to 0
-x_correction = 0 #bbox-minx
-y_correction = 0 #bbox-miny
+bbox = []
 
 scale = 1  # Enlarging the triangles on the screen
 
 # More granular correction factor, isn't affected by the scale parameter
-x_offset = 10
-y_offset = 10
+x_offset = 0
+y_offset = 0
 
 # Higher frequency is less updates, lower frequency is more updates (it's a x % frequency == 0)
-UPDATE_FREQUENCY = 1000
+UPDATE_FREQUENCY = 500
 
 # Only updates every nth triangle, increases clarity in high density datasets
 # Can also put this to 1 and make the scaling factor larger
@@ -36,7 +35,7 @@ THINNING_FACTOR = 1
 screen = pygame.display.set_mode(window_dimensions)
 screen.fill(white)
 
-pygame.display.set_caption('SSTVis')
+pygame.display.set_caption('sstvis')
 pygame.display.flip()
 
 v_count = 1
@@ -46,12 +45,19 @@ class Vertex:
     def __init__(self, x, y, z):
         # print(x,y,z)
         # print(x_correction)
-        self.x = (float(x) - x_correction) * scale + x_offset
-        self.y = (float(y) - y_correction) * scale + y_offset
+        # self.x = (float(x) - bbox[0]) * scale + x_offset
+        # self.y = (float(y) - bbox[1]) * scale + y_offset
+        self.x, self.y = transform(x, y) 
         self.z = float(z)
 
         # Easy way to check if the offsets need adjusting; if these are larger than your window_dimensions, it's not going to show up.
         # print(self.x, self.y)
+
+def transform(x, y):
+    rex = (float(x) - bbox[0]) * scale + x_offset
+    rey = (float(y) - bbox[1]) * scale + y_offset
+    return (rex, rey)
+        
 
 
 def process_line(line, count):
@@ -62,19 +68,21 @@ def process_line(line, count):
 
     elif split_line[0] == "b":
         # return
-        global x_correction 
-        x_correction = float(split_line[1])
-        global y_correction 
-        y_correction = float(split_line[2])
-
-        deltax = float(split_line[3]) - x_correction
-        deltay = float(split_line[4]) - y_correction
+        global bbox
+        bbox.append(float(split_line[1]))
+        bbox.append(float(split_line[2]))
+        bbox.append(float(split_line[3]))
+        bbox.append(float(split_line[4]))
+        deltax = bbox[2] - bbox[0]
+        deltay = bbox[3] - bbox[1]
         delta = deltax
         if (deltay < deltax):
             delta = deltay
-
         global scale
         scale = math.floor(window_dimensions[1] / delta)
+        minx, miny = transform(bbox[0], bbox[1])
+        maxx, maxy = transform(bbox[2], bbox[3])
+        pygame.draw.lines(screen, red, True, ((minx, miny), (maxx, miny), (maxx, maxy), (minx, maxy)), width=3)
 
 
     elif split_line[0] == "v":
@@ -92,7 +100,8 @@ def process_line(line, count):
         f3 = int(split_line[3])
 
         if count % THINNING_FACTOR == 0:
-            pygame.draw.lines(screen, black, True, ((vertices[f1].x, vertices[f1].y), (vertices[f2].x, vertices[f2].y), (vertices[f3].x, vertices[f3].y)))
+            pygame.draw.lines(screen, black, True, ((vertices[f1].x, vertices[f1].y), (vertices[f2].x, vertices[f2].y), (vertices[f3].x, vertices[f3].y)), width=1)
+            # pygame.draw.circle(screen, black, ((vertices[f1].x, vertices[f1].y)), 1)
 
         if count % UPDATE_FREQUENCY == 0:
             pygame.display.update()
